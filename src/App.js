c2d3d066bedde7f61aac9428e6035d50f687dc9c
bbshark99 <blue.shark99@outlook.com>
2020-04-18 22:56:05 +1000

    Add components/NewsItem, components/NewsList to show news by grid from newsapi response with axios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import NewsList from './components/NewsList';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        `https://newsapi.org/v2/top-headlines?country=us&apiKey=9e83329e629e478a81e0257fa66eae53`,
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>Download</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} />}
-    </div>
-  );
+  return <NewsList />;
 };
 
 export default App;
