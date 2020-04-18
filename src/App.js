9fab14a455ba486eee39a3ca5112a7592422d21f
bbshark99 <blue.shark99@outlook.com>
2020-04-18 22:36:30 +1000

    Add new component to show how to use axios

--- src/App.js
+++ src/App.js
@@ -1,139 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  // Run Single Job
-  function increase(number, func) {
-    setTimeout(() => {
-      const result = number + 10;
-      if (func) {
-        func(result);
-      }
-    }, 1000);
-  }
-
-  increase(0, (result) => {
-    console.log('increase: ', result);
-  });
-
-  // run 4 jobs
-  function job_thread(number, callback) {
-    setTimeout(() => {
-      const result = number + 10;
-      if (callback) {
-        callback(result);
-      }
-    }, 1000);
-  }
-
-  console.log('Job Start');
-  job_thread(0, (result) => {
-    console.log('Job1:', result);
-    job_thread(result, (result) => {
-      console.log('Job2:', result);
-      job_thread(result, (result) => {
-        console.log('Job3:', result);
-        job_thread(result, (result) => {
-          console.log('Job4:', result);
-          console.log('Job completed');
-        });
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = () => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => {
+        setData(response.data);
       });
-    });
-  });
-
-  // Promise
-  function job_runner(number) {
-    const promise = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const result = number + 10;
-        if (result > 50) {
-          const e = new Error('NumberTooBig');
-          return reject(e);
-        }
-        resolve(result);
-      }, 1000);
-    });
-    return promise;
-  }
-
-  job_runner(0)
-    .then((number) => {
-      console.log('Job Runner1:', number);
-      return job_runner(number);
-    })
-    .then((number) => {
-      console.log('Job Runner2:', number);
-      return job_runner(number);
-    })
-    .then((number) => {
-      console.log('Job Runner3:', number);
-      return job_runner(number);
-    })
-    .then((number) => {
-      console.log('Job Runner4:', number);
-      return job_runner(number);
-    })
-    .then((number) => {
-      console.log('Job Runner5:', number);
-      return job_runner(number);
-    })
-    .catch((e) => {
-      console.log('Job Runner:Error:', e);
-    });
-
-  // Async/Await
-  function increment(number) {
-    const promise = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const result = number + 10;
-        if (result > 50) {
-          const e = new Error('NumberTooBig');
-          return reject(e);
-        }
-        resolve(result);
-      }, 1000);
-    });
-    return promise;
-  }
-
-  async function runTasks() {
-    try {
-      let result = await increment(0);
-      console.log('Asyncd Job1:', result);
-      result = await increment(result);
-      console.log('Asyncd Job2:', result);
-      result = await increment(result);
-      console.log('Asyncd Job3:', result);
-      result = await increment(result);
-      console.log('Asyncd Job4:', result);
-      result = await increment(result);
-      console.log('Asyncd Job5:', result);
-      result = await increment(result);
-      console.log('Asyncd Job6:', result);
-    } catch (e) {
-      console.log('Asynced Job Error:', e);
+  };
+  function replacer(key, value) {
+    console.log(typeof value);
+    if (key === 'completed') {
+      return undefined;
+      //   return 'A';
     }
+    return value;
   }
-  runTasks();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={onClick}>Download</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, replacer, 10)}
+          readOnly={true}
+        />
+      )}
     </div>
   );
-}
+};
 
 export default App;
