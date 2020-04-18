d8293f4e6f2114aae258b7d592f4f27d04c4ffc0
bbshark99 <blue.shark99@outlook.com>
2020-04-18 22:18:13 +1000

    Add job thread to run 4 jobs per second

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  function increase(number, func) {
+    setTimeout(() => {
+      const result = number + 10;
+      if (func) {
+        func(result);
+      }
+    }, 1000);
+  }
+
+  increase(0, (result) => {
+    console.log("increase: ", result);
+  });
+
+  function job_thread(number, callback) {
+    setTimeout(() => {
+      const result = number + 10;
+      if (callback) {
+        callback(result);
+      }
+    }, 1000);
+  }
+
+  console.log("Job Start");
+  job_thread(0, (result) => {
+    console.log("Job1:", result);
+    job_thread(result, (result) => {
+      console.log("Job2:", result);
+      job_thread(result, (result) => {
+        console.log("Job3:", result);
+        job_thread(result, (result) => {
+          console.log("Job4:", result);
+          console.log("Job completed");
+        });
+      });
+    });
+  });
   return (
     <div className="App">
       <header className="App-header">
